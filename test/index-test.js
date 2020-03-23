29919f2299aa636a9c6a757d0e469a8b08d2cb70
zaid85j <zaid85j@gmail.com>
2020-03-23 17:22:06 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,8 +3,8 @@ var index = require('../index.js')
 
 describe('what-is-a-test', () => {
   describe('Name', () => {
-    it('returns "Joe"', () => {
-      expect(index.name).toEqual('Joe')
+    it('returns "Susan"', () => {
+      expect(index.name).toEqual('Susan')
     })
   })
 
